ceff83d1656236bd0662ba58d20365259cf3f320
unknown <victorio5054@gmail.com>
2022-07-26 18:20:39 -0300

    estilo / prettier

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,40 +1,49 @@
-import { useState, useEffect } from 'react';
-import { FaQuoteLeft } from 'react-icons/fa';
-import { FaQuoteRight } from 'react-icons/fa'
-import './Card.css'
+import { useState, useEffect } from "react";
+import "./Card.css";
 
 function Card() {
+  const [advices, setAdvices] = useState([]);
 
-    const [advices, setAdvices] = useState([])
+  const baseURL = "https://api.adviceslip.com/advice";
 
-    const baseURL = 'https://api.adviceslip.com/advice'
-
-    async function findAllAdvices () {
+  async function findAllAdvices() {
     const response = await fetch(baseURL);
     const advices = await response.json();
-    setAdvices([advices.slip])
-    }
-    console.log("slip",advices)
+    setAdvices([advices.slip]);
+  }
+  console.log("slip", advices);
 
-    useEffect(() => {
-        findAllAdvices()
-    }, [])
+  const handlereset = () => {
+    findAllAdvices();
+  };
 
+  useEffect(() => {
+    findAllAdvices();
+  }, []);
 
   return (
-    <div className='container_Advices'>
-        {advices.map((advice, index) => (
-        <div key={index} className='card_Advices'>
-            <p className='text_Advice'>
-                {`"${advice.advice}"`}
-            </p>  
-        </div>      
-        )
-        
-        )}
-        
+    <div className="container_Advices">
+      {advices.map((advice, index) => (
+        <div key={index} className="card_Advices">
+          <h2 className="id_Advice">{`A D V I C E # ${advice.id}`}</h2>
+          <p className="text_Advice">{`"${advice.advice}"`}</p>
+          <img
+            className="divider"
+            src="./assets/images/pattern-divider-desktop.svg"
+            alt="divisor"
+          />
+          <div className="border_Dice">
+            <img
+              className="dice"
+              onClick={handlereset}
+              src="./assets/images/icon-dice.svg"
+              alt="dice"
+            />
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,11 @@
-import Card from '../components/Card/Card';
-import './App.css'
+import Card from "../components/Card/Card";
+import "./App.css";
 
 function App() {
   return (
-  <div className="home_Page">
-      <Card/>
-  </div>
+    <div className="home_Page">
+      <Card />
+    </div>
   );
 }
 
